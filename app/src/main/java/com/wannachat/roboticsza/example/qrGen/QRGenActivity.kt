ed8e2370123b946e53907eb0529d19e7c3f3c742
roboticsza <robotics.za@gmail.com>
2018-08-01 14:37:33 +0700

    add QRCode example

---
+++ app/src/main/java/com/wannachat/roboticsza/example/qrGen/QRGenActivity.kt
@@ -0,0 +1,15 @@
+package com.wannachat.roboticsza.example.qrGen
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.wannachat.roboticsza.example.R
+
+class QRGenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qrgen)
+
+    }
+
+}
